c60f4a16a5f09c237ec0cc1a99c84fcdac3c4133
TymoteuszKaczynski <tymek.kaczynski@gmail.com>
2024-11-04 16:39:07 +0100

    message

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -2,8 +2,19 @@
 
 #include "Stocznia.hpp"
 
-unsigned int transportujFlota(unsigned int towar)
-{
-    // Twoja implementacja tutaj
+unsigned int transportujFlota(unsigned int towar){
+    if (towar==0)
     return 0;
+
+ Stocznia stocznia{}; 
+  
+     unsigned int ilosc_zaglowcow = 0;
+    while (stocznia.getTotalCap() < towar) {
+        Statek* statek1 = stocznia();
+        statek1->transportuj();
+        if (dynamic_cast<Zaglowiec*> (statek1))
+            ilosc_zaglowcow++;
+        delete statek1;
+    }
+    return ilosc_zaglowcow;   
 }
